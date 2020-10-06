a54459954b710200a2f2985e0e7fabaf91529636
Dylan Johnson <mdjohnson2@ualr.edu>
2020-10-06 16:35:45 -0500

    finished assignment 3 aside from adding country images

--- 03-android-resources/app/src/main/java/com/ualr/resources/MainActivity.java
+++ 03-android-resources/app/src/main/java/com/ualr/resources/MainActivity.java
@@ -3,14 +3,16 @@ package com.ualr.resources;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
 // TODO 3: Define the alternative resources needed to have different content and look n feel depending on the device language.
-// TODO 4: Get the description string value from resources
-// TODO 5: Initialize the text property of the TextView element with the "country_description_text" id by using the value retrieved in the previous TODO point
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +20,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView countryDescripView = findViewById(R.id.country_description_text);
+        String countryDescripString = this.getResources().getString(R.string.country_description);
+        countryDescripView.setText(countryDescripString);
+    }
+
+    public void onLessonLearntClicked(View v) {
+        String completeLessonText = this.getResources().getString(R.string.congrats_text);
+        Toast toast = Toast.makeText(this, completeLessonText, Toast.LENGTH_LONG);
+        toast.show();
     }
 
 }
