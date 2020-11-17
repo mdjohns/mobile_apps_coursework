0841ae87276222f5008b971090ed6efc21a53d28
Dylan Johnson <mdjohnson2@ualr.edu>
2020-11-17 08:09:28 -0600

    initial assignment 7 commit, added app bar

---
+++ 07-fragments/app/src/main/java/com/ualr/recyclerviewassignment/MainActivity.java
@@ -0,0 +1,69 @@
+package com.ualr.recyclerviewassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.ualr.recyclerviewassignment.Utils.DataGenerator;
+import com.ualr.recyclerviewassignment.adapters.RecyclerAdapter;
+import com.ualr.recyclerviewassignment.model.Inbox;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements RecyclerAdapter.OnItemClickListener {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int DEFAULT_POS = 0;
+
+
+    private RecyclerAdapter mAdapter;
+    private FloatingActionButton mFAB;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inbox_list_fragment);
+        initComponent();
+    }
+
+    private void initComponent() {
+        List<Inbox> inboxList = DataGenerator.getInboxData(this);
+        inboxList.addAll(DataGenerator.getInboxData(this));
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        mAdapter = new RecyclerAdapter(this, inboxList);
+        mAdapter.setOnItemClickListener(this);
+        final RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(mAdapter);
+
+        mFAB = findViewById(R.id.fab);
+        mFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Inbox newItem = DataGenerator.getRandomInboxItem(getApplicationContext());
+                mAdapter.addEmail(DEFAULT_POS, newItem);
+                recyclerView.scrollToPosition(DEFAULT_POS);
+
+            }
+        });
+    }
+
+    @Override
+    public void onItemClick(View view, Inbox obj, int position) {
+        mAdapter.clearAllSelections();
+        obj.toggleSelection();
+        mAdapter.notifyItemChanged(position);
+    }
+
+    @Override
+    public void onIconClick(View view, Inbox obj, int position) {
+        if (obj.isSelected()) {
+            mAdapter.deleteEmail(position);
+        }
+    }
+}
---
+++ 07-fragments/app/src/main/java/com/ualr/recyclerviewassignment/adapters/RecyclerAdapter.java
@@ -0,0 +1,149 @@
+package com.ualr.recyclerviewassignment.adapters;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatTextView;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.ualr.recyclerviewassignment.R;
+import com.ualr.recyclerviewassignment.model.Inbox;
+
+import java.util.List;
+
+public class RecyclerAdapter extends RecyclerView.Adapter {
+    private static final String TAG = RecyclerAdapter.class.getSimpleName();
+    private List<Inbox> mItems;
+    private Context mContext;
+    private OnItemClickListener mOnItemClickListener;
+
+    public interface OnItemClickListener {
+        void onItemClick(View view, Inbox obj, int position);
+
+        void onIconClick(View view, Inbox obj, int position);
+    }
+
+    public void setOnItemClickListener(final OnItemClickListener mInboxClickListener) {
+        this.mOnItemClickListener = mInboxClickListener;
+    }
+
+    public RecyclerAdapter(Context context, List<Inbox> items) {
+        this.mItems = items;
+        this.mContext = context;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        RecyclerView.ViewHolder vh;
+        View inboxView = LayoutInflater.from(parent.getContext()).inflate(R.layout.email_item_layout, parent, false);
+        vh = new InboxViewHolder(inboxView);
+
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, final int position) {
+        InboxViewHolder viewHolder = (InboxViewHolder) holder;
+        final Inbox inbox = mItems.get(position);
+
+        // Color resources
+        int selectedColor = mContext.getResources().getColor(R.color.grey_20);
+        int selectedIconColor = mContext.getResources().getColor(R.color.colorAccent);
+        int defaultIconColor = mContext.getResources().getColor(R.color.colorPrimary);
+
+        // Drawable resources and initializations
+        Drawable defaultCircle = mContext.getDrawable(R.drawable.shape_circle);
+        Drawable selectedIndicator = mContext.getDrawable(R.drawable.ic_delete_24px);
+        Drawable selectedCircle = mContext.getDrawable(R.drawable.shape_circle);
+        defaultCircle.mutate().setColorFilter(defaultIconColor, PorterDuff.Mode.SRC_IN);
+        selectedCircle.mutate().setColorFilter(selectedIconColor, PorterDuff.Mode.SRC_IN);
+        selectedCircle.setBounds(0, 0, 24, 24);
+
+        // Set standard content
+        viewHolder.senderIcon.setText(inbox.getFrom().substring(0, 1));
+        viewHolder.senderName.setText(inbox.getFrom());
+        viewHolder.senderEmail.setText(inbox.getEmail());
+        viewHolder.emailPreview.setText(R.string.lorem_ipsum);
+        viewHolder.emailTimestamp.setText(inbox.getDate());
+
+        if (inbox.isSelected()) {
+            viewHolder.lyt_parent.setBackgroundColor(selectedColor);
+            viewHolder.senderIcon.setBackground(selectedCircle);
+            viewHolder.senderIcon.setCompoundDrawablesWithIntrinsicBounds(selectedIndicator, null,null,null);
+
+        }
+        else {
+            viewHolder.lyt_parent.setBackgroundColor(Color.TRANSPARENT);
+            viewHolder.senderIcon.setBackground(defaultCircle);
+            viewHolder.senderIcon.setCompoundDrawablesWithIntrinsicBounds(null,null,null,null);
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.mItems.size();
+    }
+
+    public void addEmail(int position, Inbox email) {
+        mItems.add(position, email);
+        notifyItemInserted(position);
+    }
+
+    public void deleteEmail(int position) {
+        mItems.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public void clearAllSelections() {
+        for (Inbox mItem : mItems) {
+            mItem.setSelected(false);
+        }
+        notifyDataSetChanged();
+    }
+
+
+    private class InboxViewHolder extends RecyclerView.ViewHolder {
+        public TextView senderIcon;
+        public TextView senderName;
+        public TextView senderEmail;
+        public TextView emailPreview;
+        public TextView emailTimestamp;
+        public View lyt_parent;
+
+        public InboxViewHolder(View v) {
+            super(v);
+            senderIcon = v.findViewById(R.id.sender_icon);
+            senderName = v.findViewById(R.id.sender_name);
+            senderEmail = v.findViewById(R.id.sender_email);
+            emailPreview = v.findViewById(R.id.email_preview);
+            emailTimestamp = v.findViewById(R.id.email_timestamp);
+            lyt_parent = v.findViewById(R.id.lyt_parent);
+
+            senderIcon.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mOnItemClickListener.onIconClick(view, mItems.get(getLayoutPosition()), getLayoutPosition());
+                }
+            });
+            lyt_parent.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mOnItemClickListener.onItemClick(view, mItems.get(getLayoutPosition()), getLayoutPosition());
+                }
+            });
+        }
+    }
+}
+
